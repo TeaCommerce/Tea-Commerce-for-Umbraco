ac430048422762698de9bd713bdddb5c089c121e
Anders Burla Johansen <anders@burla.dk>
2015-01-21 14:44:38 +0100

    Only show that there is child elements for the Security tree when there actually is

--- Source/TeaCommerce.Umbraco.Application/Trees/SecurityTree.cs
+++ Source/TeaCommerce.Umbraco.Application/Trees/SecurityTree.cs
@@ -1,11 +1,12 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using TeaCommerce.Api.Infrastructure.Security;
 using TeaCommerce.Umbraco.Application.Resources;
 using TeaCommerce.Umbraco.Application.Utils;
-using Umbraco.Web.UI.Pages;
 using umbraco.BusinessLogic;
 using umbraco.cms.presentation.Trees;
-using TeaCommerce.Api.Infrastructure.Security;
-using System.Linq;
+using Umbraco.Web.UI.Pages;
 
 namespace TeaCommerce.Umbraco.Application.Trees {
   public class SecurityTree : ABaseTree<SecurityTree.SecurityTreeNodeType> {
@@ -18,41 +19,50 @@ namespace TeaCommerce.Umbraco.Application.Trees {
       Permissions permissions = PermissionService.Instance.GetCurrentLoggedInUserPermissions();
 
       if ( permissions != null && permissions.HasPermission( GeneralPermissionType.AccessSecurity ) ) {
-        AssignNodeValues( rootNode, GetNodeIdentifier( SecurityTreeNodeType.Security ), StoreTerms.Security, Constants.TreeIcons.Lock, "security", true );
+        AssignNodeValues( rootNode, GetNodeIdentifier( SecurityTreeNodeType.Security ), StoreTerms.Security, Constants.TreeIcons.Lock, "security", GetUsers().Any() );
       } else {
         rootNode = null;
       }
     }
 
     public override void Render( ref XmlTree tree ) {
-      Permissions permissions = PermissionService.Instance.GetCurrentLoggedInUserPermissions();
-
       switch ( CurrentNodeType ) {
         case SecurityTreeNodeType.Security:
-          User currentUser = umbraco.helper.GetCurrentUmbracoUser();
-
-          //Show all users if current user is admin or has access to users section
-          bool showAllUsers = currentUser != null && ( currentUser.IsRoot() || currentUser.Applications.Any( a => a.alias == "users" ) );
-
-          foreach ( User user in User.getAll() ) {
-            if ( user.IsRoot() || user.Applications.All( a => a.alias != "teacommerce" ) ) continue; //Don't ever show admin user
-
-            bool showUser = showAllUsers || user.Id.ToString( CultureInfo.InvariantCulture ) == permissions.UserId;
-
-            //If user still doesn't have access - then only show users that has access to the same store as current user
-            if ( !showUser ) {
-              Permissions userPermissions = PermissionService.Instance.Get( user.Id.ToString( CultureInfo.InvariantCulture ) );
-              showUser = userPermissions != null && permissions.StoreSpecificPermissions.Any( p => p.Value.HasFlag( StoreSpecificPermissionType.AccessStore ) && userPermissions.HasPermission( StoreSpecificPermissionType.AccessStore, p.Key ) );
-            }
-
-            if ( showUser ) {
-              XmlTreeNode node = CreateNode( GetNodeIdentifier( SecurityTreeNodeType.User, user.Id ), user.Name, Constants.TreeIcons.User, "user" );
-              node.Action = "javascript:(function(){" + ClientTools.Scripts.ChangeContentFrameUrl( WebUtils.GetPageUrl( Constants.Pages.EditUserPermissions ) + "?id=" + user.Id ) + "})";
-              tree.Add( node );
-            }
+          foreach ( User user in GetUsers() ) {
+            XmlTreeNode node = CreateNode( GetNodeIdentifier( SecurityTreeNodeType.User, user.Id ), user.Name, Constants.TreeIcons.User, "user" );
+            node.Action = "javascript:(function(){" + ClientTools.Scripts.ChangeContentFrameUrl( WebUtils.GetPageUrl( Constants.Pages.EditUserPermissions ) + "?id=" + user.Id ) + "})";
+            tree.Add( node );
           }
           break;
       }
+    }
+
+    private IEnumerable<User> GetUsers() {
+      List<User> users = new List<User>();
+
+      Permissions permissions = PermissionService.Instance.GetCurrentLoggedInUserPermissions();
+      User currentUser = umbraco.helper.GetCurrentUmbracoUser();
+
+      //Show all users if current user is admin or has access to users section
+      bool showAllUsers = currentUser != null && ( currentUser.IsRoot() || currentUser.Applications.Any( a => a.alias == "users" ) );
+
+      foreach ( User user in User.getAll() ) {
+        if ( user.IsRoot() || user.Applications.All( a => a.alias != "teacommerce" ) ) continue; //Don't ever show admin user
+
+        bool showUser = showAllUsers || user.Id.ToString( CultureInfo.InvariantCulture ) == permissions.UserId;
+
+        //If user still doesn't have access - then only show users that has access to the same store as current user
+        if ( !showUser ) {
+          Permissions userPermissions = PermissionService.Instance.Get( user.Id.ToString( CultureInfo.InvariantCulture ) );
+          showUser = userPermissions != null && permissions.StoreSpecificPermissions.Any( p => p.Value.HasFlag( StoreSpecificPermissionType.AccessStore ) && userPermissions.HasPermission( StoreSpecificPermissionType.AccessStore, p.Key ) );
+        }
+
+        if ( showUser ) {
+          users.Add( user );
+        }
+      }
+
+      return users;
     }
 
     public enum SecurityTreeNodeType {
