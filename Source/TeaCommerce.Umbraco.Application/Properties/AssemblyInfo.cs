1bf0c8c2eaf89c0e2153d8d15948a4bad3bd3923
Rune Gronkjaer <rg@teasolutions.dk>
2016-08-05 11:26:06 +0200

    Downgraded Newtonsoft binaries to lowest common denominator for Umbraco versions. This Newtonsoft is used in Umbraco v6.1+ and v7.1 Newtonsoft will from now on not be shipped with Tea Commerce Bumped version number

--- Source/TeaCommerce.Umbraco.Application/Properties/AssemblyInfo.cs
+++ Source/TeaCommerce.Umbraco.Application/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using TeaCommerce.Umbraco.Application;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion( "3.0.0.0" )]
-[assembly: AssemblyFileVersion( "3.1.1.0" )]
+[assembly: AssemblyFileVersion( "3.1.2.0" )]
 
 //Embedded Resources
 
