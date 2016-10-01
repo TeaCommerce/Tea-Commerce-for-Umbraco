f4b87336a2d82fc973c7b15c7eb9cf5e64ae3af0
Anders Burla Johansen <anders@burla.dk>
2016-10-01 15:06:04 +0200

    Build using Visual Studio 2015 msbuild

--- Build/Package.build.debug.bat
+++ Build/Package.build.debug.bat
@@ -1,3 +1,3 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Publish.build.xml /property:Configuration=Debug
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" Publish.build.xml /property:Configuration=Debug
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" Package.build.xml
 @pause
