3bd00e6174099378c159fd3edacd8abcfbaffe18
mattbrailsford <me@mattbrailsford.com>
2019-01-27 16:09:30 +0000

    Looking at implementing cache refresher based on events, but looks like we might need some more events.

--- Source/TeaCommerce.Umbraco.Application/ApplicationStartup.cs
+++ Source/TeaCommerce.Umbraco.Application/ApplicationStartup.cs
@@ -1,32 +1,56 @@
 ﻿using System.Globalization;
 using System.Linq;
+using TeaCommerce.Api.Notifications;
 using Umbraco.Core;
 using Umbraco.Core.Events;
 using Umbraco.Core.Models;
 using Umbraco.Core.Services;
+using Umbraco.Web.Cache;
 
-namespace TeaCommerce.Umbraco.Application {
+namespace TeaCommerce.Umbraco.Application
+{
 
-  public class ApplicationStartup : ApplicationEventHandler {
+    public class ApplicationStartup : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            NotificationCenter.Store.Created += (s, e) => { InvalidateStoresCache(); };
+            NotificationCenter.Store.Deleted += (s, e) => { InvalidateStoresCache(); };
 
-    protected override void ApplicationStarted( UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext ) {
-      ContentService.Copying += ContentService_Copying;
+            NotificationCenter.Order.Created += (o, e) => { InvalidateOrdersCache(o.StoreId); };
+            NotificationCenter.Order.Updated += (o, e) => { InvalidateOrdersCache(o.StoreId); };
+            NotificationCenter.Order.Deleted += (o, e) => { InvalidateOrdersCache(o.StoreId); };
+
+            ContentService.Copying += ContentService_Copying;
+        }
+
+        private void InvalidateStoresCache()
+        {
+            DistributedCache.Instance.RefreshAll(Constants.DistributedCache.StoreCacheRefresherGuid);
+        }
+
+        private void InvalidateOrdersCache(long storeId)
+        {
+            DistributedCache.Instance.Refresh(Constants.DistributedCache.OrderCacheRefresherGuid, (int)storeId);
+        }
+
+        private void ContentService_Copying(IContentService sender, CopyEventArgs<IContent> e)
+        {
+            Property masterRelationProperty = e.Copy.Properties.SingleOrDefault(p => p.Alias == Api.Constants.ProductPropertyAliases.MasterRelationPropertyAlias);
+
+            if (masterRelationProperty == null || (masterRelationProperty.Value != null && !string.IsNullOrEmpty(masterRelationProperty.Value.ToString())))
+            {
+                return;
+            }
+
+            // Delete all property data
+            foreach (Property property in e.Copy.Properties)
+            {
+                property.Value = null;
+            }
+
+            masterRelationProperty.Value = e.Original.Id.ToString(CultureInfo.InvariantCulture);
+        }
+
     }
-
-    void ContentService_Copying( IContentService sender, CopyEventArgs<IContent> e ) {
-      Property masterRelationProperty = e.Copy.Properties.SingleOrDefault( p => p.Alias == Api.Constants.ProductPropertyAliases.MasterRelationPropertyAlias );
-
-      if ( masterRelationProperty == null || ( masterRelationProperty.Value != null && !string.IsNullOrEmpty( masterRelationProperty.Value.ToString() ) ) ) {
-        return;
-      }
-
-      //Delete all property data
-      foreach ( Property property in e.Copy.Properties ) {
-        property.Value = null;
-      }
-
-      masterRelationProperty.Value = e.Original.Id.ToString( CultureInfo.InvariantCulture );
-    }
-
-  }
 }
