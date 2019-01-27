dc86ee2fc25ba8b67cd15e7eba2789281b9f7fba
mattbrailsford <me@mattbrailsford.com>
2019-01-27 19:04:13 +0000

    Setup event handlers to refresh distributed caches

--- Source/TeaCommerce.Umbraco.Application/ApplicationStartup.cs
+++ Source/TeaCommerce.Umbraco.Application/ApplicationStartup.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Linq;
 using TeaCommerce.Api.Notifications;
 using Umbraco.Core;
@@ -9,29 +10,76 @@ using Umbraco.Web.Cache;
 
 namespace TeaCommerce.Umbraco.Application
 {
-
     public class ApplicationStartup : ApplicationEventHandler
     {
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
-            NotificationCenter.Store.Created += (s, e) => { InvalidateStoresCache(); };
-            NotificationCenter.Store.Deleted += (s, e) => { InvalidateStoresCache(); };
+            SetupDistributedCacheInvalidation();
+            SetupContentCopyingHandler();
+        }
 
-            NotificationCenter.Order.Created += (o, e) => { InvalidateOrdersCache(o.StoreId); };
-            NotificationCenter.Order.Updated += (o, e) => { InvalidateOrdersCache(o.StoreId); };
-            NotificationCenter.Order.Deleted += (o, e) => { InvalidateOrdersCache(o.StoreId); };
+        private void SetupDistributedCacheInvalidation()
+        {
+            NotificationCenter.Campaign.Created += (e) => { InvalidateDistributedCache(Constants.DistributedCache.CampaignCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.Campaign.Updated += (e) => { InvalidateDistributedCache(Constants.DistributedCache.CampaignCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.Campaign.Deleted += (e) => { InvalidateDistributedCache(Constants.DistributedCache.CampaignCacheRefresherGuid, e.StoreId); };
 
+            NotificationCenter.Country.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CountryCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.Country.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CountryCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.Country.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CountryCacheRefresherGuid, e.StoreId); };
+
+            NotificationCenter.CountryRegion.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CountryRegionCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.CountryRegion.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CountryRegionCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.CountryRegion.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CountryRegionCacheRefresherGuid, e.StoreId); };
+
+            NotificationCenter.Currency.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CurrencyCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.Currency.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CurrencyCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.Currency.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.CurrencyCacheRefresherGuid, e.StoreId); };
+
+            NotificationCenter.EmailTemplate.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.EmailTemplateCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.EmailTemplate.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.EmailTemplateCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.EmailTemplate.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.EmailTemplateCacheRefresherGuid, e.StoreId); };
+
+            NotificationCenter.Order.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.OrderCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.Order.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.OrderCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.Order.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.OrderCacheRefresherGuid, e.StoreId); };
+
+            NotificationCenter.OrderStatus.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.OrderStatusCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.OrderStatus.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.OrderStatusCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.OrderStatus.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.OrderStatusCacheRefresherGuid, e.StoreId); };
+
+            NotificationCenter.PaymentMethod.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.PaymentMethodCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.PaymentMethod.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.PaymentMethodCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.PaymentMethod.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.PaymentMethodCacheRefresherGuid, e.StoreId); };
+
+            NotificationCenter.ShippingMethod.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.ShippingMethodCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.ShippingMethod.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.ShippingMethodCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.ShippingMethod.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.ShippingMethodCacheRefresherGuid, e.StoreId); };
+
+            NotificationCenter.Store.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.StoreCacheRefresherGuid); };
+            NotificationCenter.Store.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.StoreCacheRefresherGuid); };
+            NotificationCenter.Store.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.StoreCacheRefresherGuid); };
+
+            NotificationCenter.VatGroup.Created += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.VatGroupCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.VatGroup.Updated += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.VatGroupCacheRefresherGuid, e.StoreId); };
+            NotificationCenter.VatGroup.Deleted += (e, a) => { InvalidateDistributedCache(Constants.DistributedCache.VatGroupCacheRefresherGuid, e.StoreId); };
+        }
+
+        private void InvalidateDistributedCache(Guid cacheKey, long? storeId = null)
+        {
+            if (storeId.HasValue)
+            {
+                DistributedCache.Instance.Refresh(cacheKey, (int)storeId);
+            }
+            else
+            {
+                DistributedCache.Instance.RefreshAll(cacheKey);
+            }
+        }
+
+        private void SetupContentCopyingHandler()
+        {
             ContentService.Copying += ContentService_Copying;
-        }
-
-        private void InvalidateStoresCache()
-        {
-            DistributedCache.Instance.RefreshAll(Constants.DistributedCache.StoreCacheRefresherGuid);
-        }
-
-        private void InvalidateOrdersCache(long storeId)
-        {
-            DistributedCache.Instance.Refresh(Constants.DistributedCache.OrderCacheRefresherGuid, (int)storeId);
         }
 
         private void ContentService_Copying(IContentService sender, CopyEventArgs<IContent> e)
