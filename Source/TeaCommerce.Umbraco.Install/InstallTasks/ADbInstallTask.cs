68ebd5dbf53be30fe97cf5245ec6dfc4b52107fd
mattbrailsford <me@mattbrailsford.com>
2019-06-18 09:18:34 +0100

    Updated data type install task to support inserting with fixed IDs to help with umbraco cloud deployment issues

---
+++ Source/TeaCommerce.Umbraco.Install/InstallTasks/ADbInstallTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Umbraco.Core;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace TeaCommerce.Umbraco.Install.InstallTasks
+{
+    public abstract class ADbInstallTask : IInstallTask
+    {
+        protected UmbracoDatabase Database => ApplicationContext.Current.DatabaseContext.Database;
+
+        protected ISqlSyntaxProvider SqlSyntax = SqlSyntaxContext.SqlSyntaxProvider;
+
+        public abstract void Install();
+
+        public abstract void Uninstall();
+
+        protected void WithIdentityInsert(string tableName, Action act)
+        {
+            if (SqlSyntax.SupportsIdentityInsert())
+                Database.Execute(new Sql(string.Format("SET IDENTITY_INSERT {0} ON ", SqlSyntax.GetQuotedTableName(tableName))));
+
+            act.Invoke();
+
+            if (SqlSyntax.SupportsIdentityInsert())
+                Database.Execute(new Sql(string.Format("SET IDENTITY_INSERT {0} OFF ", SqlSyntax.GetQuotedTableName(tableName))));
+        }
+    }
+}
